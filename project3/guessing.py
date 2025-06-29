e45e045293acd53c4af1e840400a3ae2acecf81c
first file <ja052119@gmail.com>
2025-06-29 20:05:07 +0530

    initial cmmit

--- project3/guessing.py
+++ project3/guessing.py
@@ -1,2 +1,39 @@
-print("HELLO WORLD")
-age = 18
+import random
+
+easy_word = ["india", "pakistan", "japan", "kohli", "rohit", "iran", "thai" ,"tiger"]
+medium_word = ["big boy", "roundup", "undertaker", "roman rance", "eliphant", "starboy"]
+hard_word = ["this is godguy","out of ten" ,"increament salery", "t20 world cup","Noboduy argument me", "i am the best"]
+
+print("welcome the passoword guessing game with hints")
+print ("choose a difficulty level : easy , medium or hard")
+
+level = input("Enter difficulty level : ").lower()
+
+if level == "easy" :
+    secret = random.choice(easy_word)
+elif level == "medium":
+    secret = random.choice(medium_word)   
+elif level == "hard" :
+    secret = random.choice(hard_word)
+else :
+    print ("Enter the valid choice : defaulting chosing easy level")
+    secret = random.choice(easy_word)
+
+attempts = 0
+print ("\n guess the secrate password !")
+
+while True:
+    guess = input("Enter the guessing keyword :").lower()
+    attempts += 1
+    if guess == secret:
+        print (f"congratulations! you guessed it in {attempts} attempts. ")
+        break
+    hint = ""
+
+    for i in range(len(secret)):
+        if i < len(guess) and guess[i] == secret[i]:
+            hint += guess[i]
+        else :
+            hint += "_"
+    print ("Hint : ", hint)
+print("Game over")
